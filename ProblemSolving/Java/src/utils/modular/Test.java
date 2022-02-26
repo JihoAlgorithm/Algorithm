bbac036dce8d5eb8515726850b33a9c624b87a21
Jihogrammer <jiho@kakao.com>
2022-02-26 12:05:51 +0900

    test: 1000 단위로 끊어서 24씩 더해야 하는 듯

--- ProblemSolving/Java/src/utils/modular/Test.java
+++ ProblemSolving/Java/src/utils/modular/Test.java
@@ -3,10 +3,10 @@ package utils.modular;
 class Test {
 
     public static void main(String[] args) {
-        for (int i = 999; i < 2000; i++) {
-            int k = (i + 24) & 1023;
+        for (int i = 2000; i < 3000; i++) {
+            int k = (i + 48) & 1023;
             int s = i % 1000;
-            System.out.println("k: " + k + ", s: " + s);
+            System.out.println("i: " + i + ", k: " + k + ", s: " + s);
         }
     }
 
