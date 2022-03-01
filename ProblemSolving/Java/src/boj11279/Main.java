14a105df0043086877229d78dcaeddc952741b82
Jihogrammer <jiho@kakao.com>
2022-03-01 23:54:31 +0900

    solve: BOJ 11279 최대 힙
    - Heap
    - 빨리 풀려고 예전에 작성한 게시글 참고함
    - 게시글 내용이 구리네
    - https://jihogrammer.tistory.com/7
    - http://boj.kr/9d50f5af53584a76978b329f0c07f624

---
+++ ProblemSolving/Java/src/boj11279/Main.java
@@ -0,0 +1,60 @@
+package boj11279;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        Heap heap = new Heap(N);
+        StringBuilder sb = new StringBuilder();
+        while (N-- > 0) {
+            int c = read();
+            if (c == 0) {
+                sb.append(heap.poll()).append('\n');
+                continue;
+            }
+            heap.offer(c);
+        }
+        System.out.print(sb);
+    }
+
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+}
+
+class Heap {
+    int[] heap;
+    int size;
+    Heap(int size) {
+        heap = new int[size + 1];
+    }
+    void offer(int e) {
+        heap[++size] = e;
+        int i = size << 1;
+        while ((i >>= 1) > 1)
+            if (!swap(i)) break;
+    }
+    int poll() {
+        if (size == 0) return 0;
+        int e = heap[1];
+        heap[1] = heap[size--];
+        int i = 1;
+        while ((i <<= 1) <= size) {
+            if (i < size && heap[i + 1] > heap[i]) i++;
+            if (!swap(i)) break;
+        }
+        return e;
+    }
+    boolean swap(int i) {
+        int j = i >> 1;
+        int p = heap[j];
+        int c = heap[i];
+        if (p > c) return false;
+        heap[j] = c;
+        heap[i] = p;
+        return true;
+    }
+}
