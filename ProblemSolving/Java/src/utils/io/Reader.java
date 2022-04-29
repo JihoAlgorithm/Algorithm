202ad02088f463aedf7aede8602168f0ae9f129e
Jihogrammer <jiho@kakao.com>
2022-04-29 13:57:26 +0900

    ✨ update static readInt

--- ProblemSolving/Java/src/utils/io/Reader.java
+++ ProblemSolving/Java/src/utils/io/Reader.java
@@ -52,4 +52,18 @@ public class Reader {
         return buffer[index++];
     }
 
+    public static int readUInt() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    public static int readInt() throws Exception {
+        int c, n = System.in.read() & 15;
+        boolean isNegative = n == 13;
+        if (isNegative) n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return isNegative ? ~n + 1 : n;
+    }
+
 }
