c27d933987ccf03d78245a96e497509ed41303b7
Jihogrammer <jiho@kakao.com>
2022-05-24 09:35:17 +0900

    ✅ BOJ 20353 Atrium
    #Geometry

---
+++ ProblemSolving/Java/src/boj20353/Main.java
@@ -0,0 +1,27 @@
+package boj20353;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(sqrt(read()) * 4);
+    }
+
+    static final int PRECISION = 30;
+
+    static double sqrt(double n) {
+        double x = PRECISION;
+
+        for (int i = 0; i < PRECISION; i++)
+            x = 0.5 * (n / x + x);
+
+        return x;
+    }
+
+    static long read() throws Exception {
+        int c;
+        long n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+}
