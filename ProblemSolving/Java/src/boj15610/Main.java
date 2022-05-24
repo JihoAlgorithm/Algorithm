b8e6250925546c2e030ed1c517e037a0a0914ae7
Jihogrammer <jiho@kakao.com>
2022-05-24 11:39:50 +0900

    ✅ BOJ 15610 Abbey Courtyard
    #Geometry

---
+++ ProblemSolving/Java/src/boj15610/Main.java
@@ -0,0 +1,27 @@
+package boj15610;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.print(sqrt(read()) * 4);
+    }
+
+    static final int PRECISION = 28;
+
+    static double sqrt(double n) {
+        double x = PRECISION;
+
+        for (int i = 0; i < PRECISION; i++)
+            x = 0.5 * (n / x + x);
+
+        return x;
+    }
+
+    static long read() throws Exception {
+        int c;
+        long n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+}
