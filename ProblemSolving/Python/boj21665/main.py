1f4a99f21c4dc7e427dc997e3833ffe05025d801
Jihogrammer <jihogrammer@gmail.com>
2022-08-14 16:01:17 +0900

    ✅ BOJ 21665 Миша и негатив
    #Implementation #String

--- ProblemSolving/Python/boj21665/main.py
+++ ProblemSolving/Python/boj21665/main.py
@@ -1,12 +1,3 @@
 N, M = map(int, input().split())
-before = [input() for _ in range(N)]
-input()
-after = [input() for _ in range(N)]
-
-# count = 0
-
-# for r1, r2 in zip(before, after):
-#     for c1, c2 in zip(r1, r2):
-#         count += c1 == c2
-
-print(sum(c1 == c2 for r1, r2 in zip(before, after) for c1, c2 in zip(r1, r2)))
+P = [[input() for _ in range(N)], input(), [input() for _ in range(N)]]
+print(sum(c[0] == c[1] for r in zip(P[0], P[2]) for c in zip(*r)))
