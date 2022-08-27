5441f68b97f90c19c3bdd99697136e91d15b2477
Jihogrammer <jihogrammer@gmail.com>
2022-08-27 17:58:47 +0900

    ✅ BOJ 2745 진법 변환
    - 아,,,
    N, B = input().split()
    print(int(N, int(B)))
    
    #String #Implementation #Mathematics

--- ProblemSolving/Python/boj2745/main.py
+++ ProblemSolving/Python/boj2745/main.py
@@ -1,10 +1,10 @@
-s, B = input().split()
+N, B = input().split()
 B = int(B)
-e = len(s) - 1
+e = len(N) - 1
 
 answer = 0
 
-for i, c in enumerate(s):
+for i, c in enumerate(N):
     n = ((ord(c) & 31) + 9) if ord(c) > 64 else (ord(c) & 15)
     answer += n * (B ** (e - i))
 
