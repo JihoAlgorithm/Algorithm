52cb2df803bdabec23d9ff136a6d9a99ecb13d9c
Jihogrammer <jihogrammer@gmail.com>
2022-09-17 22:02:43 +0900

    🧹 rename a variable

--- ProblemSolving/Python/boj1855/main.py
+++ ProblemSolving/Python/boj1855/main.py
@@ -1,4 +1,4 @@
 K = int(input())
 p = input()
 rows = [p[j : j + K][:: [1, -1][i & 1]] for i, j in enumerate(range(0, len(p), K))]
-print(*["".join(row) for row in zip(*rows)], sep="")
+print(*["".join(col) for col in zip(*rows)], sep="")
